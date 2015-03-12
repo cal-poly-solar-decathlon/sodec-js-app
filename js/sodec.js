d776b63515f48a8c60f7a299a17b9ece75e27c7f
John Clements <clements@racket-lang.org>
2015-03-12 13:27:09 -0700

    got CORS worked out

--- js/sodec.js
+++ js/sodec.js
@@ -1,14 +1,32 @@
 SLOME = (function (){
 
-  function handleTimestamp(data){
-    alert('success!');
+  // copied from stackoverflow
+  // http://stackoverflow.com/questions/1219860/html-encoding-in-javascript-jquery/1219983#1219983
+  function htmlEscape(str) {
+    return String(str)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
   }
-  
+
+  function updateTimestamp(data){
+    $('#timestamp').html(data.timestamp);
+  }
+
+  function updateBedTemp(data){
+    $('#bed-temp').html("" + (data.status / 1000.0) + " degrees" );
+  }
+
+  var host = 'calpolysolardecathlon.org'
   function go(){
-    alert('ready to goxxx');
     $.ajax(
-      {url: 'http://localhost:8080/srv/timestamp',
-       success: handleTimestamp,
+      {url: 'http://'+host+':8080/srv/timestamp',
+       success: updateTimestamp,
+       dataType: 'json'});
+    $.ajax({url: 'http://'+host+':8080/srv/latest-event?device=s-temp-bed',
+       success: updateBedTemp,
        dataType: 'json'});
   }
 
@@ -18,6 +36,7 @@ SLOME = (function (){
 $( document ).ready(function() {
   SLOME.go();
 
+  var intervalID = window.setInterval(SLOME.go, 1000);
   //GeneratePassword.start();
   // set up a handler for the go button
   /*$( "#gobutton" ).click(function( event ) {
