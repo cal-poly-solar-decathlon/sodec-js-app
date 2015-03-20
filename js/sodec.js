7d100b773775a77be935efa08cd521a6c3228443
John Clements <clements@racket-lang.org>
2015-03-19 17:33:52 -0700

    daily

--- js/sodec.js
+++ js/sodec.js
@@ -1,5 +1,7 @@
 SLOME = (function (){
 
+  var DAYSECONDS = 86400;
+
   // copied from stackoverflow
   // http://stackoverflow.com/questions/1219860/html-encoding-in-javascript-jquery/1219983#1219983
   function htmlEscape(str) {
@@ -12,29 +14,80 @@ SLOME = (function (){
   }
 
   function updateTimestamp(data){
-    $('#timestamp').html(data.timestamp);
+    $('#timestamp').html(data);
   }
 
   function updateBedTemp(data){
-    $('#bed-temp').html("" + (data.status / 1000.0) + " degrees" );
+    $('#bed-temp').html("" + (data.status / 10.0) + " degrees" );
   }
 
-  var host = 'calpolysolardecathlon.org'
+  // construct sodec server url
+  var HOST = 'calpolysolardecathlon.org'
+  var PORT = 8080;
+  function pathToURL(path){
+    return 'http://'+HOST+':'+PORT+"/srv"+path;
+  }
+
+  // send a request to the sodec server asynchronously
+  function makeRequest(path,successFun){
+    $.ajax({url: pathToURL(path),
+            success: successFun,
+            dataType: 'json'});
+  }
+
+  // get the timestamp, hand it off to the continuation
+  function getTimestamp(kont){
+    makeRequest('/timestamp',
+                function(data){
+                  return kont(ensureInteger(data.timestamp));
+                });
+  }
+
+  // only numbers get through. Contract system, anyone?
+  function ensureInteger(x){
+    if (Number.isInteger(x)){
+      return x;
+    } else {
+      console.log("expected number, got: "+x);
+      // what the heck do we return here? Grr, javascript...
+      alert('ac');
+    }
+  }
+
   function go(){
-    $.ajax(
-      {url: 'http://'+host+':8080/srv/timestamp',
-       success: updateTimestamp,
-       dataType: 'json'});
-    $.ajax({url: 'http://'+host+':8080/srv/latest-event?device=s-temp-bed',
-       success: updateBedTemp,
-       dataType: 'json'});
+    getTimestamp(updateTimestamp);
+    //makeRequest('/timestamp',updateTimestamp);
+    makeRequest('/latest-event?device=s-temp-bed',updateBedTemp);
   }
 
-  return {go : go}
+  // handle a click on the refresh button
+  function refreshButton(){
+    getTimestamp(
+      function(ts){
+        makeRequest(
+          '/events-in-range?device=s-temp-bed?start='+(ts-DAYSECONDS)+';end='+ts,
+          function(data){
+            $('#numevents').html(data.seriesData.length)
+          })
+      });
+    /*$.ajax({url: 'http://'+host+':8080/srv/events-in-range?device=s-temp-bed',
+       success: lastHoursData,
+       dataType: 'json'});*/
+    alert('ding!');
+  }
+
+  return {go : go,
+          refreshButton: refreshButton}
 }())
 
 $( document ).ready(function() {
   SLOME.go();
+
+  // set up a handler for the refresh button
+  $( "#refreshbutton" ).click(function( event ) {
+    SLOME.refreshButton();
+    event.preventDefault();
+  });
 
   var intervalID = window.setInterval(SLOME.go, 1000);
   //GeneratePassword.start();
