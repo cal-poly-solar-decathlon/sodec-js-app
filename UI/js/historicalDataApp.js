f9934f1c2ee0cb58aad520663d3c647fb5190a7d
Shubham Kahal <shubhamkahal@gmail.com>
2015-09-17 21:45:24 -0700

    Made UI Fixes

---
+++ UI/js/historicalDataApp.js
@@ -0,0 +1,18 @@
+/**
+ * Created by shubhamkahal on 9/17/2015.
+ */
+angular.module("SolarHouseHistoricalDataApp", ['ngRoute'])
+    .config(function($routeProvider){
+        $routeProvider
+            .when('/tempHistory', {templateUrl:'../partials/tempHistory.html'})
+            .when('/humHistory', {templateUrl:'../partials/humHistory.html'})
+            .when('/elecUseHistory', {templateUrl:'../partials/elecUseHistory.html'})
+            .when('/lightLevelHistory', {templateUrl:'../partials/lightLevelHistory.html'})
+            .when('/occHistory', {templateUrl:'../partials/occHistory.html'})
+            .otherwise({redirectTo:'/historicalData', templateUrl:'..partials/home.html'})
+    })
+    .controller('viewController', ['$scope', '$location', function($scope, $location) {
+        $scope.setRoute = function(route){
+            $location.path(route);
+        }
+    }]);
